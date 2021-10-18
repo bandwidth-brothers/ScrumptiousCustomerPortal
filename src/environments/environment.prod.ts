a19771155c41b27183f79e8c295bef24092e3756
joshuagreenss <87674718+joshuagreenss@users.noreply.github.com>
2021-10-18 10:26:59 -0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
+import { NgxLoggerLevel } from "ngx-logger";
+
 export const environment = {
-  production: true
+  production: false,
+  baseAuthUrl: 'http://localhost:8080/auth',
+  USERS_GET_ALL_URL: '/accounts',
+  USERS_GET_URL: '/accounts/',
+  BASE_CUSTOMER_URL: 'http://localhost:8080/customer',
+  CUSTOMERS_GET_URL: '/me/',
+  logLevel: NgxLoggerLevel.TRACE,
+  serverLogLevel: NgxLoggerLevel.OFF
 };
